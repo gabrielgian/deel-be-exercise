c8028e416e242bb3e9ff411f336501eb495744d6
gabrielgian <gabriel.tessaroli@gmail.com>
2022-11-21 18:22:14 -0300

    feat: add best Client route

--- src/routes/Admin.routes.js
+++ src/routes/Admin.routes.js
@@ -35,8 +35,45 @@ const getBestProfession = async (req, res) => {
   res.status(200).json({ profession: profession?.Contract?.Contractor?.profession }).end();
 };
 
+const getBestClients = async (req, res) => {
+  const { Contract, Job, Profile } = req.app.get('models');
+  const sequelize = req.app.get('sequelize');
+
+  const { start, end, limit = 2 } = req.query;
+
+  if (!start || !end) {
+    res.status(404).json().end();
+  }
+
+  const groupedJobsByClient = await Job.findAll({
+    attributes: ['Contract.Client.*', [sequelize.fn('sum', sequelize.col('price')), 'sum_price']],
+    include: {
+      model: Contract,
+      include: {
+        model: Profile,
+        as: 'Client',
+      },
+    },
+    where: {
+      paid: true,
+      paymentDate: {
+        [Op.gte]: start,
+        [Op.lte]: end,
+      },
+    },
+    group: 'Contract.Client.id',
+    order: [['sum_price', 'DESC']],
+    limit,
+  });
+
+  const clients = groupedJobsByClient.map((client) => client.Contract?.Client);
+
+  res.status(200).json(clients).end();
+};
+
 const adminRoutes = (app) => {
   app.get('/admin/best-profession', getProfile, getBestProfession);
+  app.get('/admin/best-clients', getProfile, getBestClients);
 };
 
 export default adminRoutes;
--- src/test/Admin.spec.js
+++ src/test/Admin.spec.js
@@ -16,9 +16,36 @@ describe('Testing Admin routes', async () => {
         .set('profile_id', 1)
         .end((_, res) => {
           res.should.have.status(200);
-          console.log(res.body);
           res.body.should.be.an('object');
           res.body.should.have.property('profession').eq('Programmer');
+          done();
+        });
+    });
+  });
+
+  describe('GET: /admin/best-clients?start=<date>&end=<date>&limit=<integer>', () => {
+    it('Should return the best clients between the specified interval', (done) => {
+      chai
+        .request(app)
+        .get('/admin/best-clients?start=2020-01-01&end=2021-11-01&limit=4')
+        .set('profile_id', 1)
+        .end((_, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.property('length').eq(4);
+          done();
+        });
+    });
+
+    it('Should return the best clients between the specified interval using the default limit', (done) => {
+      chai
+        .request(app)
+        .get('/admin/best-clients?start=2020-01-01&end=2021-11-01')
+        .set('profile_id', 1)
+        .end((_, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.have.property('length').eq(2);
           done();
         });
     });
