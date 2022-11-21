7f4b650c5d594ff2e763ed38c9dc8d6e59e2bbbc
gabrielgian <gabriel.tessaroli@gmail.com>
2022-11-21 17:05:19 -0300

    feat: add paid job route

--- src/routes/Job.routes.js
+++ src/routes/Job.routes.js
@@ -28,8 +28,68 @@ const getUnpaidJobs = async (req, res) => {
   res.status(200).json(jobs).end();
 };
 
+const postPayJob = async (req, res) => {
+  try {
+    const { Contract, Job, Profile } = req.app.get('models');
+    const sequelize = req.app.get('sequelize');
+
+    const { job_id: jobId } = req.params;
+    const job = await Job.findOne({
+      where: { id: jobId },
+      include: {
+        model: Contract,
+        include: [
+          { model: Profile, as: 'Client' },
+          { model: Profile, as: 'Contractor' },
+        ],
+      },
+    });
+
+    if (!job) {
+      return res.status(404).json().end();
+    }
+
+    const jobCanBePaid = () => {
+      if (job.paid) {
+        return false;
+      }
+
+      if (job.Contract.status === 'terminated') {
+        return false;
+      }
+
+      if (job.price > job.Contract.Client.balance) {
+        return false;
+      }
+
+      return true;
+    };
+
+    if (!jobCanBePaid()) {
+      return res.status(403).json().end();
+    }
+
+    const paidJob = await sequelize.transaction(async (transaction) => {
+      const newClientBalance = job.Contract.Client.balance - job.price;
+      const newContractorBalance = job.Contract.Contractor + job.price;
+
+      await job.Contract.Client.update({ balance: newClientBalance }, { transaction });
+      await job.Contract.Contractor.update({ balance: newContractorBalance }, { transaction });
+      return await job.update({ paid: true, paymentDate: new Date() }, { transaction });
+    });
+
+    res.status(200).json(paidJob).end();
+
+  } catch (error) {
+    
+    res.status(500).json().end();
+  
+  }
+};
+
 const contractRoutes = (app) => {
   app.get('/jobs/unpaid', getProfile, getUnpaidJobs);
+  app.post('/jobs/:job_id/pay', getProfile, postPayJob);
 };
 
 export default contractRoutes;
--- src/test/Job.spec.js
+++ src/test/Job.spec.js
@@ -34,4 +34,53 @@ describe('Testing Jobs routes', () => {
           });
       });
   });
+
+  describe('POST: /jobs/:job_id/pay', () => {
+    it('Should pay an job when client has enough balance', (done) => {
+      chai
+        .request(app)
+        .post('/jobs/2/pay')
+        .set('profile_id', 1)
+        .end((_, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.be.an('object');
+          done();
+        });
+    });
+
+    it('Should return error when client doesn;\'t have enough balance to pay', (done) => {
+      chai
+        .request(app)
+        .post('/jobs/5/pay')
+        .set('profile_id', 1)
+        .end((_, res) => {
+          res.should.have.status(403);
+          done();
+        });
+    });
+
+    it('Should return error when job is already paid', (done) => {
+      chai
+        .request(app)
+        .post('/jobs/6/pay')
+        .set('profile_id', 1)
+        .end((_, res) => {
+          res.should.have.status(403);
+          done();
+        });
+    });
+
+    it('Should return error when contract is terminated', (done) => {
+      chai
+        .request(app)
+        .post('/jobs/1/pay')
+        .set('profile_id', 6)
+        .end((_, res) => {
+          res.should.have.status(403);
+          done();
+        });
+    });
+
+  });
 });
